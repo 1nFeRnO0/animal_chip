b105ffee6331308c7a9963511c13e9379dfdc5e0
Anton Antonov <mine.minefis@gmail.com>
2023-03-15 14:04:13 +0400

    add entity class

---
+++ alpha/src/main/resources/Organization.java
@@ -0,0 +1,9 @@
+public class Organization {
+    private String locationName;
+    private String Address;
+
+    public Organization(){
+        this.locationName = null;
+        this.Address = null;
+    }
+}
