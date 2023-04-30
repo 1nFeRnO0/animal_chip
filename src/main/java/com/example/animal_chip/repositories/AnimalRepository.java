ac8d631f940cac4a50d97c3e4cbca6ee5ca8ef5f
zenn <mine.minefis@gmail.com>
2023-04-30 18:59:04 +0400

    added search by type/name/point

--- src/main/java/com/example/animal_chip/repositories/AnimalRepository.java
+++ src/main/java/com/example/animal_chip/repositories/AnimalRepository.java
@@ -3,9 +3,20 @@ package com.example.animal_chip.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.animal_chip.models.Animal;
 
+
+
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
-    List<Animal> findByType(String type);
+    //List<Animal> findByName(String type);
+    public enum criteria{
+        type,
+        name,
+        point
+    }
+    
+    @Query("Select a from Animal a where p.?2 like %?1%")
+    List<Animal> findAll(String prompt, criteria c);
 }
--- src/main/java/com/example/animal_chip/services/AnimalService.java
+++ src/main/java/com/example/animal_chip/services/AnimalService.java
@@ -22,7 +22,7 @@ public class AnimalService {
     private final AnimalRepository animalRepository;
 
     public List<Animal> listAnimals(String type) {
-        if (type != null) return animalRepository.findByType(type);
+        if (type != null) return animalRepository.findAll(type, AnimalRepository.criteria.type);
         return animalRepository.findAll();
     }
 
